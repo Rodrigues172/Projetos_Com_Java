1f78543d90e34c36889e2e4fc3d040b142df103a
Davi Rodrigues <davi.rodrigues170218@gmail.com>
2025-04-29 22:25:34 -0300

    Criacao do Projeto

---
+++ primeiro_programa_java/src/executavel_java/PrimeiroClasseJava.java
@@ -0,0 +1,13 @@
+package executavel_java;
+
+public class PrimeiroClasseJava {
+
+	
+	/*Main e um metodo auto excutavel*/
+	public static void main(String[] args) {
+		
+		/*se fosse cout no c++*/
+		System.out.println("Meu primeiro Programa em java no curso java web!");
+		System.out.println("Java e !");
+	}
+}

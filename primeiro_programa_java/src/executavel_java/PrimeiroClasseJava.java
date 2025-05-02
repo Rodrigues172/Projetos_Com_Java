94211f70418e3ff3717b7c3cfddf8bfebf2faea8
Rodrigues172 <davi.rodrigues170218@gmail.com>
2025-05-02 00:15:14 -0300

    test hith matematica

--- primeiro_programa_java/src/executavel_java/PrimeiroClasseJava.java
+++ primeiro_programa_java/src/executavel_java/PrimeiroClasseJava.java
@@ -7,8 +7,9 @@ public class PrimeiroClasseJava {
 		int nota1 = 70;
 		int nota2 = 80;
 		int nota3 = 20;
-		int nota4 = 30
+		int nota4 = 30;
 			
-		system
+		System.out.println("A Soma dos numeros das natas sao : " + (nota1 + nota2 + nota3 + nota4 ));
+		System.out.println("A Medias dos das notas sao : " + ((nota1 + nota2 + nota3 + nota4) / 4));
 	}
 }

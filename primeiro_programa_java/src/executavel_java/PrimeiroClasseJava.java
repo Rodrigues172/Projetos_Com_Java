4625148c82c8fdae22f555386046e27a2d65a0f5
Rodrigues172 <davi.rodrigues170218@gmail.com>
2025-05-04 02:21:17 -0300

    programa de media de nota

--- primeiro_programa_java/src/executavel_java/PrimeiroClasseJava.java
+++ primeiro_programa_java/src/executavel_java/PrimeiroClasseJava.java
@@ -7,27 +7,28 @@ public class PrimeiroClasseJava {
 	/* Main e um metodo auto excutavel */
 	public static void main(String[] args) {
 
-		String carros = JOptionPane.showInputDialog("Imforme a quantidade de carros?");
-		String pessoas = JOptionPane.showInputDialog("Imforme a quantidade de carros?");
+		String nota1 = JOptionPane.showInputDialog("Informe a nota 1");
+		String nota2 = JOptionPane.showInputDialog("Informe a nota 2");
+		String nota3 = JOptionPane.showInputDialog("Informe a nota 3");
+		String nota4 = JOptionPane.showInputDialog("Informe a nota 4");
 
-		double carroNumero = Double.parseDouble(carros);
-		double pessoasNumero = Double.parseDouble(pessoas);
+		double dNota1 = Double.parseDouble(nota1);
+		double dNota2 = Double.parseDouble(nota2);
+		double dNota3 = Double.parseDouble(nota3);
+		double dNota4 = Double.parseDouble(nota4);
 
-		int divisao = (int) (carroNumero / pessoasNumero);
+		double media = ((dNota1 + dNota2 + dNota3 + dNota4) / 4);
 
-		double resto = carroNumero % pessoasNumero;
-
-		int resposta = JOptionPane.showConfirmDialog(null, "Deseja ver o resultado da divisao?");
-
-		if (resposta == 0) {
-			JOptionPane.showMessageDialog(null, "Divisao para pessoas deu " + divisao);
-
-		}
-
-		resposta = JOptionPane.showConfirmDialog(null, "Deseja ver o resto da conta?");
-
-		if (resposta == 0) {
-			JOptionPane.showMessageDialog(null, "O resto da divisao e " + resto + " Carros");
+		if (media >= 50) {
+			if (media >= 90) {
+				JOptionPane.showMessageDialog(null, "Parabens voce foi aprovado com acima de media: " + media);
+			} else if (media >= 70) {
+				JOptionPane.showMessageDialog(null, "Parabens voce foi aprovado com a media: " + media);
+			} else {
+				JOptionPane.showMessageDialog(null, "Ha nao voce esta de recuperacao com a media: " + media);
+			}
+		} else {
+			JOptionPane.showMessageDialog(null, "Voce foi reprovado com media abaixo de 50 sua media foi: " + media);
 		}
 
 	}

7bbaa69da38db70023a8d1f57fbcb62cc2f2c070
Rodrigues172 <davi.rodrigues170218@gmail.com>
2025-05-04 01:50:55 -0300

    test

--- primeiro_programa_java/src/executavel_java/PrimeiroClasseJava.java
+++ primeiro_programa_java/src/executavel_java/PrimeiroClasseJava.java
@@ -2,21 +2,33 @@ package executavel_java;
 
 import javax.swing.JOptionPane;
 
-public class PrimeiroClasseJava {	
-	
-	/*Main e um metodo auto excutavel*/
+public class PrimeiroClasseJava {
+
+	/* Main e um metodo auto excutavel */
 	public static void main(String[] args) {
-		
+
 		String carros = JOptionPane.showInputDialog("Imforme a quantidade de carros?");
 		String pessoas = JOptionPane.showInputDialog("Imforme a quantidade de carros?");
-		
+
 		double carroNumero = Double.parseDouble(carros);
 		double pessoasNumero = Double.parseDouble(pessoas);
-		
-		int divisao = (int) (carroNumero / pessoasNumero); 
-		
+
+		int divisao = (int) (carroNumero / pessoasNumero);
+
 		double resto = carroNumero % pessoasNumero;
-		
-		JOptionPane.showMessageDialog(null, "Divisao para pessoas deu " + divisao + " carros e sobrou " + resto + " Carros");
+
+		int resposta = JOptionPane.showConfirmDialog(null, "Deseja ver o resultado da divisao?");
+
+		if (resposta == 0) {
+			JOptionPane.showMessageDialog(null, "Divisao para pessoas deu " + divisao);
+
+		}
+
+		resposta = JOptionPane.showConfirmDialog(null, "Deseja ver o resto da conta?");
+
+		if (resposta == 0) {
+			JOptionPane.showMessageDialog(null, "O resto da divisao e " + resto + " Carros");
+		}
+
 	}
 }
